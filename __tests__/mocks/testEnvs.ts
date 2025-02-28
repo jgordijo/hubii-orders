fc8a55b18421f63e31c025d75c1abb07beaec3ba
Julio Gordijo <jgordijo@gmail.com>
2025-02-28 15:07:36 -0300

    test: add unit test mocks and test for hello world route

---
+++ __tests__/mocks/testEnvs.ts
@@ -0,0 +1,4 @@
+process.env.PORT = '3334';
+process.env.DATABASE_URL = 'postgresql://DATABASE_URL';
+process.env.PRODUCTS_API_URL = 'http://localhost:3333';
+process.env.WAREHOUSE_ZIPCODE = '09030300';
