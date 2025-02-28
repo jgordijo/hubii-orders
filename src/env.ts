453aac3980f225a093bd1e3a67380f11f1f09f65
Julio Gordijo <jgordijo@gmail.com>
2025-02-28 15:05:14 -0300

    chore: update gitgnore, create tsup.config, jest.config and .env.example

--- src/env.ts
+++ src/env.ts
@@ -4,6 +4,7 @@ const envSchema = z.object({
   PORT: z.coerce.number().default(3334),
   DATABASE_URL: z.string().url(),
   PRODUCTS_API_URL: z.string().url(),
+  WAREHOUSE_ZIPCODE: z.string(),
 });
 
 export const env = envSchema.parse(process.env);
